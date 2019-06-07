91ef2fa953852f3a750c7b85fc5062b344ff8dd1
Benny Lee <benny.lee@msn.com>
2019-06-07 18:26:25 +0000

    adding products now show up in home page

--- dance_project/urls.py
+++ dance_project/urls.py
@@ -18,23 +18,24 @@ from django.contrib import admin
 from dance_app.views import index, login, logout, vip_view_function
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from cart import urls as urls_cart
+from search import urls as urls_search
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
-from cart import urls as urls_cart
-from search import urls as urls_search
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls, name="admin_link"),
-    url(r'^$', index, name="index_link"),
+    url(r'^$', all_products, name='index'),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
     url(r'^login$', login, name="login_link"),
     url(r'^logout$', logout, name="logout_link"),
     url(r'^vip$', vip_view_function, name="vip_link"),
-    url(r'^accounts/', include(urls_accounts)),
-    url(r'^products/', include(urls_products)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
-    url(r'^cart/', include(urls_cart)),
-    url(r'^search/', include(urls_search)),
 ]
 
 
