9635962e4caab3cbed47ad83bc02d4f96bfb83bb
Benny Lee <benny.lee@msn.com>
2019-06-12 02:22:05 +0000

    modify settings back before trying to make travis work

--- dance_project/settings.py
+++ dance_project/settings.py
@@ -94,7 +94,7 @@ else:
     }
 
 
-# DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
