2b48c420e68e216246e39b87f484a8634c52c174
Benny Lee <benny.lee@msn.com>
2019-06-12 15:25:06 +0000

    edit custom.css

--- cart/contexts.py
+++ cart/contexts.py
@@ -16,6 +16,11 @@ def cart_contents(request):
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
+        
+        total_with_commas = ("{:,}".format(total)) # try display with commas
+        print("total_with_commas = ", total_with_commas) # try display with commas
+        print(type(total_with_commas)) # try display with commas
+        
         product_count += quantity
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
     
