f6c8ba4f127ce477ba29e67354c3c97085dd3fb8
Benny Lee <benny.lee@msn.com>
2019-06-16 21:53:51 +0000

    change stripe charge in checkout app  to 1 to easily see exact increase in stripe website dashboard. settings env commented out for heroku

--- checkout/views.py
+++ checkout/views.py
@@ -40,7 +40,7 @@ def checkout(request):
             
             try:
                 customer = stripe.Charge.create(
-                    amount=int(total * 423),
+                    amount=int(total * 1),
                     currency="SGD",
                     description=request.user.email,
                     card=payment_form.cleaned_data['stripe_id']
